c781084f3a513fc2a9d06c472619c9663dd6b9d6
brent-sakihara <brent.saki@gmail.com>
2020-10-06 17:02:49 -0700

    adding final unit tests to function

--- test.cpp
+++ test.cpp
@@ -7,6 +7,34 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, Spaces) {
+	char* test_val[3]; test_val[0] = "./c-echo";
+	test_val[1] = " "; 
+	test_val[2] = " ";
+	EXPECT_EQ("   ", echo(3, test_val));
+}
+
+TEST (EchoTest, Numbers) {
+	char* test_val[4]; test_val[0] = "./c-echo";
+	test_val[1] = "8";
+	test_val[2] = "23";
+	test_val[3] = "1";
+	EXPECT_EQ("8 23 1", echo(4, test_val));
+}
+
+TEST(EchoTest, SpecialCharacters) {
+	char* test_val[3];
+	test_val[0] = "./c-echo";
+	test_val[1] = "@#";
+	test_val[2] = "*~\\";
+	EXPECT_EQ("@# *~\\", echo(3, test_val));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
